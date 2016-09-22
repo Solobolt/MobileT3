b4d5080f2470ebe95abb054fe39ec41a52fe50e9
SAE\ghaage <solobolt@gmail.com>
2016-09-22 17:58:13 +1000

    add Marble script add Marble Handler Script

---
+++ Assets/MarbleSolitaire/Scripts/Marble.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Marble : MonoBehaviour {
+
+    public enum MarbleState
+    {
+        CLEAR,
+        NONE,
+        MARBLE
+    }
+
+    public MarbleState state = MarbleState.CLEAR;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MarbleSolitaire/Scripts/MarbleHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarbleHandler : MonoBehaviour {
+
+    public Marble[,] marbles = new Marble[10,10];
+	// Use this for initialization
+	void Start () {
+        marbles[2, 2].state = Marble.MarbleState.MARBLE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
