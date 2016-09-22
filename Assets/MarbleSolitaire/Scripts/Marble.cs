c5940cdf6dd8e5ce3da5027c7597a0b416419008
SAE\ghaage <solobolt@gmail.com>
2016-09-22 20:21:22 +1000

    fixed marble movement

--- Assets/MarbleSolitaire/Scripts/Marble.cs
+++ Assets/MarbleSolitaire/Scripts/Marble.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 
@@ -10,16 +11,44 @@ public class Marble : MonoBehaviour {
         NONE,
         MARBLE
     }
+    private Color marbleColor = Color.blue;
+    private Color clearColor = Color.white;
+    private Color noneThere = Color.grey;
 
     public MarbleState state = MarbleState.CLEAR;
+    public PlayerController2 player;
 
     // Use this for initialization
     void Start () {
-	
+        changeColors();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void MarbleClicked()
+    {
+        player.AddMarble(this.gameObject.GetComponent<Marble>());
+    }
+
+    public void changeColors()
+    {
+        switch (state)
+        {
+            case MarbleState.CLEAR:
+                this.gameObject.GetComponent<Image>().color = clearColor;
+                break;
+
+            case MarbleState.MARBLE:
+                this.gameObject.GetComponent<Image>().color = marbleColor;
+                break;
+
+            case MarbleState.NONE:
+                this.gameObject.GetComponent<Image>().color = noneThere;
+                break;
+        }
+    }
+    
 }
--- Assets/MarbleSolitaire/Scripts/PlayerController2.cs
+++ Assets/MarbleSolitaire/Scripts/PlayerController2.cs
@@ -3,10 +3,15 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController2 : MonoBehaviour {
-    private float spacing = 35;
+
+    public Marble[] allMarbles;
+
     public Marble currentMarble;
+    private int curNum;
     public Marble middleMarble;
+    private int midNum;
     public Marble targetMarble;
+    private int tarNum;
     public bool marbleSelected = false;
 
 	// Use this for initialization
@@ -19,22 +24,99 @@ public class PlayerController2 : MonoBehaviour {
 	 
 	}
 
-    void MarblePressed(Marble mar)
+    public void AddMarble(Marble mar)
     {
         if(marbleSelected == false)
         {
-            currentMarble = mar;
-            marbleSelected = true;
+            if(mar.state == Marble.MarbleState.MARBLE)
+            {
+                currentMarble = mar;
+                targetMarble = null;
+                marbleSelected = true;
+                int.TryParse(mar.gameObject.name, out curNum);
+                print("current: " + curNum);
+            }
+            
         }
         else
         {
             targetMarble = mar;
             marbleSelected = false;
+            int.TryParse(mar.gameObject.name, out tarNum);
+            print("current: " + curNum);
+            print("target: " + tarNum);
+
+            PlayCheck();
         }
+
+        
+    }
+
+
+    void CheckMiddle()
+    {
+        if (curNum % 5 != 4 && curNum % 5 != 3)
+        {
+            if (tarNum == curNum + 2)
+            {
+                midNum = (curNum + 1);
+                if (allMarbles[midNum].state == Marble.MarbleState.MARBLE)
+                {
+                    SetMarbles();
+                }
+            }
+        }
+
+        if (curNum % 5 != 1 && curNum % 5 != 0)
+        {
+            if (tarNum == curNum - 2)
+            {
+                midNum = (curNum - 1);
+                if (allMarbles[midNum].state == Marble.MarbleState.MARBLE)
+                {
+                    SetMarbles();
+                }
+            }
+        }
+        
+
+        if (tarNum == curNum + 10)
+        {
+            midNum = (curNum + 5);
+            if (allMarbles[midNum].state == Marble.MarbleState.MARBLE)
+            {
+                SetMarbles();
+            }
+        }
+
+        if (tarNum == curNum - 10)
+        {
+            midNum = (curNum - 5);
+            if (allMarbles[midNum].state == Marble.MarbleState.MARBLE)
+            {
+                SetMarbles();
+            }
+        }
+    }
+
+    void SetMarbles()
+    {
+        allMarbles[midNum].state = Marble.MarbleState.CLEAR;
+        allMarbles[midNum].changeColors();
+
+        allMarbles[curNum].state = Marble.MarbleState.CLEAR;
+        allMarbles[curNum].changeColors();
+
+        allMarbles[tarNum].state = Marble.MarbleState.MARBLE;
+        allMarbles[tarNum].changeColors();
     }
 
     void PlayCheck()
     {
+        if (allMarbles[curNum].state == Marble.MarbleState.MARBLE && allMarbles[tarNum].state == Marble.MarbleState.CLEAR)
+        {
+            CheckMiddle();
+        }
 
     }
 }
