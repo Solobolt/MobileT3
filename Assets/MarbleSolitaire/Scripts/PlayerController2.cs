b840d378535c0c7d088dfbb380f2ccb1744a7ba8
SAE\ghaage <solobolt@gmail.com>
2016-09-22 18:21:12 +1000

    added PlayerCOntroller2

---
+++ Assets/MarbleSolitaire/Scripts/PlayerController2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController2 : MonoBehaviour {
+    private float spacing = 35;
+    public Marble currentMarble;
+    public Marble middleMarble;
+    public Marble targetMarble;
+    public bool marbleSelected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+	}
+
+    void MarblePressed(Marble mar)
+    {
+        if(marbleSelected == false)
+        {
+            currentMarble = mar;
+            marbleSelected = true;
+        }
+        else
+        {
+            targetMarble = mar;
+            marbleSelected = false;
+        }
+    }
+
+    void PlayCheck()
+    {
+
+    }
+}
